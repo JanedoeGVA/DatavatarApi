9fd87f5c1e8382709a1d5e210d51b6dcebd5b374
JanedoeGVA <xavier.costa@unige.ch>
2018-11-12 15:56:18 +0100

    modify access token URL

--- src/main/java/outils/Constant.java
+++ src/main/java/outils/Constant.java
@@ -60,8 +60,8 @@ public class Constant {
 	public static final String STRAVA_PROVIDER = "Strava";
 	
 	public static final String STRAVA_BASE_URL = "https://www.strava.com/oauth/authorize";
-	public static final String STRAVA_TOKEN_ENDPOINT_URL = "https://api.fitbit.com/oauth2/token";
-	public static final String STRAVA_REVOKE_ENDPOINT_URL= "https://api.fitbit.com/oauth2/revoke";
+	public static final String STRAVA_TOKEN_ENDPOINT_URL = "https://www.strava.com/oauth/token";
+	//public static final String STRAVA_REVOKE_ENDPOINT_URL= "https://api.fitbit.com/oauth2/revoke";
     
     public static final String STRAVA_CALLBACK_URL = "datavatarapp://strava/oauth2/verification";
 	
