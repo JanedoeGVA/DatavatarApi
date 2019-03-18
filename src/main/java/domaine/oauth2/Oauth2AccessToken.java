7064d76a7ebe9538df82f6d8e85539c1728f307d
JanedoeGVA <xavier.costa@unige.ch>
2019-03-18 14:17:09 +0100

    adding annotation

--- src/main/java/domaine/oauth2/Oauth2AccessToken.java
+++ src/main/java/domaine/oauth2/Oauth2AccessToken.java
@@ -8,7 +8,9 @@ public class Oauth2AccessToken extends OauthAccessToken {
 	
 	@XmlElement(name="refreshTokenKey")
 	private String refreshTokenKey;
-	
+	private String test = "toto";
+	@XmlElement(name="test3")
+	private String test2 = "tit";
 	public Oauth2AccessToken() {super();}
 	
 	public Oauth2AccessToken(String accessTokenKey, String refreshTokenKey) {
