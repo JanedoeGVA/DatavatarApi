99946ae6d1a73559f97aa2ec06356dc86eb0a288
JanedoeGVA <xavier.costa@unige.ch>
2019-03-19 11:25:06 +0100

    adding xmltype

--- src/main/java/domaine/ActivityTracker.java
+++ src/main/java/domaine/ActivityTracker.java
@@ -1,44 +1,33 @@
 package domaine;
 
-import javax.xml.bind.annotation.XmlElement;
-
 import domaine.authorization.Token;
 
-
 public class ActivityTracker {
-	
+
 	private String provider;
-	private String protocol;
-	
 	private Token token;
-	
+
 	public ActivityTracker() {};
-	
-	public ActivityTracker(String provider, String protocol, Token token) {
+
+	public ActivityTracker(String provider, Token token) {
 		this.provider = provider;
-		this.protocol = protocol;
 		this.token = token;
 	}
-	
+
 	public String getProvider() {
 		return provider;
 	}
 	public void setProvider(String provider) {
 		this.provider = provider;
 	}
-	public String getProtocol() {
-		return protocol;
-	}
-	public void setProtocol(String protocol) {
-		this.protocol = protocol;
-	}
+
 	public Token getToken() {
 		return token;
 	}
 	public void setToken(Token token) {
 		this.token = token;
 	}
-	
-	
+
+
 
 }
