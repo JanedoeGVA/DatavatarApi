f930a0d58e87475d5add95e3437d617cfc695d88
JanedoeGVA <xavier.costa@unige.ch>
2019-03-19 09:37:17 +0100

    adding xmltype

--- src/main/java/domaine/oauth/OauthAccessToken.java
+++ src/main/java/domaine/oauth/OauthAccessToken.java
@@ -1,12 +1,14 @@
 package domaine.oauth;
 
+
 import javax.xml.bind.annotation.XmlSeeAlso;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlTransient;
 
 import domaine.oauth1a.Oauth1AccessToken;
 import domaine.oauth2.Oauth2AccessToken;
 
 @XmlSeeAlso({Oauth2AccessToken.class, Oauth1AccessToken.class})
+@XmlTransient
 public abstract class OauthAccessToken {
 	
 	private String key;
